7e7dc642e0f65df7ce702f45315c79dda3c67d00
yippykiyay <36166623+yippykiyay@users.noreply.github.com>
2018-05-01 10:15:30 -0500

    5/1 In Class

--- Schrodinger_exercise.m
+++ Schrodinger_exercise.m
@@ -16,29 +16,29 @@ imethod=menu('Select method:','Explicit FTCS','Implicit FTCS','Crank-Nicolson');
 
 %% * Set up the Hamiltonian operator matrix
 ham = zeros(N);  % Set all elements to zero
-coeff = _______;
+coeff = -h_bar^2/(2*mass);
 for i=2:(N-1)
-  ham(i,i-1) = _____;
-  ham(i,i) = _____; %plus V(i) % Set interior rows
-  ham(i,i+1) = _____;
+  ham(i,i-1) = coeff;
+  ham(i,i) = -2*coeff; %plus V(i) % Set interior rows
+  ham(i,i+1) = coeff;
 end
 % First and last rows for periodic boundary conditions
-ham(1,N) = _____;   ham(1,1) = _____; ham(1,2) = _____;
-ham(N,N-1) = _____; ham(N,N) = _____; ham(N,1) = _____;
+ham(1,N) = coeff;   ham(1,1) = -2*coeff; ham(1,2) = coeff;
+ham(N,N-1) = coeff; ham(N,N) = -2*coeff; ham(N,1) = coeff;
 
 if(imethod==1)     % Explicit FTCS matrix
     A = (eye(N) - i_imag*tau/h_bar*ham);
 elseif(imethod==2) % Implicit FTCS
-    A = _________________;
+    A = inv(eye(N) + i_imag*tau/h_bar*ham);
 else               % Crank-Nicolson
-    A = __________________;
+    A = inv(eye(N) + i_imag*tau/(2*h_bar)*ham)*(eye(N) - i_imag*tau/(2*h_bar)*ham);
 end
 			 
 %% * Initialize the wavefunction 
 x0 = 0;          % Location of the center of the wavepacket
 velocity = 0.5;  % Average velocity of the packet
-p0 = _____;
-k0 = _____;       % Average wavenumber
+p0 = mass*velocity;
+k0 = p0/h_bar;       % Average wavenumber
 sigma0 = L/10;   % Standard deviation of the wavefunction
 
 Norm_psi = 1/(sqrt(sigma0*sqrt(pi)));  % Normalization
@@ -68,7 +68,7 @@ drawnow; pause;
 %% * Initialize loop and plot variables 
 max_iter = L/(velocity*tau);      % Particle should circle system
 plot_iter = max_iter/50;          % Produce 20 curves
-p_plot(:,1) = ________;     % Record initial condition
+p_plot(:,1) = psi.*conj(psi);     % Record initial condition
 iplot = 1;
 
 figure(3); clf;
@@ -82,12 +82,12 @@ pause;
 for iter=1:max_iter
 	
   %* Compute new wave function based on the update matrix A
-  psi = ______;  
+  psi = A*psi;  
   
   %* Periodically record values for plotting
   if( rem(iter,plot_iter) < 1 )   
     iplot = iplot+1;
-    p_plot(:,iplot) = ________; 
+    p_plot(:,iplot) = psi.*conj(psi); 
     
     figure(2);
     plot3(xt,yt,real(psi),'-',xt(1),yt(1),real(psi(1)),'ro');
@@ -112,7 +112,7 @@ end
 %% * Plot probability versus position at various times
 figure(4); clf;
 axisV = [-L/2 L/2 0 max(p_plot)]; % Fix axis min and max
-pFinal = ________;
+pFinal = psi.*conj(psi);
 plot(x,p_plot(:,1:3:iplot),x,pFinal);    
 xlabel('x'); ylabel('P(x,t)');
 title('Probability density at various times');
